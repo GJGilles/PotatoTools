ea8851f599e6381eca8101d43a479fd1970fa713
turtttle <gjgilles@uwaterloo.ca>
2021-09-05 22:47:44 -0400

    position data for characters

--- Runtime/Character/CharacterService.cs
+++ Runtime/Character/CharacterService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace PotatoTools.Character
@@ -7,13 +8,28 @@ namespace PotatoTools.Character
     public static class CharacterService
     {
         public static UnityEvent<CharacterObject, DialogObject> OnDialog = new UnityEvent<CharacterObject, DialogObject>();
+        public static UnityEvent<CharacterObject> OnMove = new UnityEvent<CharacterObject>();
         
         private static List<CharacterObject> characters = new List<CharacterObject>();
+        private static Dictionary<CharacterObject, Vector2> positions = new Dictionary<CharacterObject, Vector2>();
 
         static CharacterService()
         {
             characters = AssetLoader.LoadObjects<CharacterObject>();
+
+            // TODO: Save position data
             FileService.Add(new Data().GetService());
+        }
+
+        public static Vector2 GetPosition(CharacterObject character)
+        {
+            return positions[character];
+        }
+
+        public static void SetPosition(CharacterObject character, Vector2 pos)
+        {
+            positions[character] = pos;
+            OnMove.Invoke(character);
         }
 
         public class Data : DataService<Dictionary<int, List<int>>>
--- Runtime/Character/NPCController.cs
+++ Runtime/Character/NPCController.cs
@@ -10,6 +10,22 @@ namespace PotatoTools.Character
         {
             selectable.character = character;
             anim.anim.runtimeAnimatorController = character.animator;
+
+            CharacterService.SetPosition(character, transform.position);
+            CharacterService.OnMove.AddListener(Move);
+        }
+
+        private void OnDestroy()
+        {
+            CharacterService.OnMove.RemoveListener(Move);
+        }
+
+        private void Move(CharacterObject c)
+        {
+            if (character == c)
+            {
+                transform.position = CharacterService.GetPosition(c);
+            }
         }
     }
 }
